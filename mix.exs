5dd27b301389491571aaaba37684c811b485c167
Garrett George <garrett.michael.george@gmail.com>
2022-02-14 10:32:06 -0600

    doc: set up hex.pm and exdoc config

--- mix.exs
+++ mix.exs
@@ -1,10 +1,15 @@
 defmodule ElixirStartingPoint.MixProject do
   use Mix.Project
 
+  @version "0.1.0"
+
   def project do
     [
       app: :zipzip,
-      version: "0.1.0",
+      version: @version,
+      description: description(),
+      name: "ZipZip",
+      docs: docs(),
       build_path: "_build",
       deps_path: "deps",
       lockfile: "mix.lock",
@@ -12,6 +17,8 @@ defmodule ElixirStartingPoint.MixProject do
       consolidate_protocols: Mix.env() != :test,
       deps: deps(),
       test_coverage: [tool: ExCoveralls],
+      package: package(),
+      source_url: "https://github.com/garrettmichaelgeorge/zip_zip",
       preferred_cli_env: [
         coveralls: :test,
         "coveralls.detail": :test,
@@ -33,10 +40,32 @@ defmodule ElixirStartingPoint.MixProject do
     []
   end
 
+  defp description,
+    do: "A zipper implementation in Elixir"
+
+  defp docs do
+    [
+      main: "ZipZip",
+      # logo: "path/to/logo.png",
+      extras: ["README.md"]
+    ]
+  end
+
+  # https://hex.pm/docs/publish#adding-metadata-to-code-classinlinemixexscode
+  defp package do
+    [
+      name: "zip_zip",
+      organization: "hexpm",
+      licenses: ["MIT"],
+      links: %{"GitHub" => "https://github.com/garrettmichaelgeorge/zip_zip"}
+    ]
+  end
+
   defp deps do
     [
       {:credo, "~> 1.6.1", only: [:dev, :test], runtime: false},
       {:dialyxir, "~> 1.1", only: [:dev, :test], runtime: false},
+      {:ex_doc, "~> 0.27", only: :dev, runtime: false},
       {:excoveralls, "~> 0.14", only: [:dev, :test], runtime: false}
     ]
   end
